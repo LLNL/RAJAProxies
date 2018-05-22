687213fd7a19d009ecf2a3524bfa1c30b88f64c8
Olga T. Pearce <pearce8@llnl.gov>
2018-05-21 18:09:11 -0700

    Adding a new partially-RAJAfied version of CoMD (in src-threaded) that works with current RAJA, along with CMake changes

---
+++ CoMD/src-threaded/CoMD_info.h
@@ -0,0 +1,15 @@
+#ifndef CoMD_info_hpp
+#define CoMD_info_hpp
+
+#define CoMD_VARIANT "CoMD-mpi"
+#define CoMD_HOSTNAME "sierra654"
+#define CoMD_KERNEL_NAME "'Linux'"
+#define CoMD_KERNEL_RELEASE "'2.6.32-573.26.1.1chaos.ch5.4.x86_64'"
+#define CoMD_PROCESSOR "'x86_64'"
+
+#define CoMD_COMPILER "'/usr/local/bin/mpiCC'"
+#define CoMD_COMPILER_VERSION "'g++ (GCC) 4.4.7 20120313 (Red Hat 4.4.7-16)'"
+#define CoMD_CFLAGS "'-std=c++03 -Wno-deprecated -D__STDC_CONSTANT_MACROS -DCALIPER_FOUND -DCGAL_FOUND -frounding-math -DDOUBLE -DDO_MPI -g -O5 -isystem/g/g14/pearce8/opt/overdecomposition/chaos_5_x86_64_ib/include -isystem/collab/usr/global/tools/talanton/opt/caliper/chaos_5_x86_64_ib/include  -isystem/usr/global/tools/adept/boost-1.46.0/chaos_5_x86_64_ib/include/boost-1.46.0 -isystem/collab/usr/global/tools/adept/CGAL-4.1/chaos_5_x86_64_ib/include'"
+#define CoMD_LDFLAGS "' -lm -L/g/g14/pearce8/opt/overdecomposition/chaos_5_x86_64_ib/lib -lgraph_vertex -loverdecomposition -L/collab/usr/global/tools/talanton/opt/caliper/chaos_5_x86_64_ib/lib -lcaliper -lcaliper-common -lcaliper-mpiwrap -L/usr/global/tools/adept/boost-1.46.0/chaos_5_x86_64_ib/lib/boost-1.46.0 -lboost_serialization-mt -L/collab/usr/global/tools/adept/CGAL-4.1/chaos_5_x86_64_ib/lib -lCGAL'"
+
+#endif
